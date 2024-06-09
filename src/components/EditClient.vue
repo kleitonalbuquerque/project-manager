91d514076c7a56b1e856f3c9583d32df3b5011d8
kleiton.silva <kleiton.silva@yduqs.com.br>
2024-06-09 17:55:14 -0300

    Crud completo

--- src/components/EditClient.vue
+++ src/components/EditClient.vue
@@ -1,50 +1,81 @@
 <template>
-    <div class="container">
-      <h1>Edit Client</h1>
-      <form @submit.prevent="handleUpdateClient">
-        <div class="form-group">
-          <label for="name">Name</label>
-          <input type="text" class="form-control" id="name" v-model="name" required>
+  <div class="container">
+    <h1>Edit Client</h1>
+    <form @submit.prevent="handleUpdateClient">
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name" v-model="name" required>
+      </div>
+      <div class="form-group">
+        <label>Projects</label>
+        <div v-for="project in projects" :key="project.id" class="form-check">
+          <input 
+            type="checkbox" 
+            class="form-check-input" 
+            :id="`project-${project.id}`" 
+            :value="project.id" 
+            :checked="isProjectSelected(project.id)"
+            @change="toggleProjectSelection(project.id)"
+          >
+          <label class="form-check-label" :for="`project-${project.id}`">{{ project.name }}</label>
         </div>
-        <div class="form-group">
-          <label>Projects</label>
-          <div v-for="project in projects" :key="project.id" class="form-check">
-            <input type="checkbox" class="form-check-input" :id="`project-${project.id}`" :value="project.id" v-model="selectedProjects">
-            <label class="form-check-label" :for="`project-${project.id}`">{{ project.name }}</label>
-          </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Update</button>
-      </form>
-    </div>
-  </template>
+      </div>
+      <button type="submit" class="btn btn-primary">Update</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { getClient, updateClient as updateClientService } from '../services/ClientService';
+import { getProjects } from '../services/ProjectService';
+import { useRoute, useRouter } from 'vue-router';
+
+const name = ref('');
+const projects = ref([]);
+const selectedProjects = ref([]);
+const route = useRoute();
+const router = useRouter();
+
+const isProjectSelected = (projectId) => {
+  return selectedProjects.value && selectedProjects.value.includes(projectId);
+};
+
+const toggleProjectSelection = (projectId) => {
+  if (!selectedProjects.value) {
+    selectedProjects.value = [];
+  }
+  const index = selectedProjects.value.indexOf(projectId);
+  if (index > -1) {
+    selectedProjects.value.splice(index, 1);
+  } else {
+    selectedProjects.value.push(projectId);
+  }
+};
+
+onMounted(async () => {
+  const clientId = route.params.id;
+  const clientResponse = await getClient(clientId);
+  const projectResponse = await getProjects();
   
-  <script setup>
-  import { ref, onMounted } from 'vue';
-  import { getClient, updateClient as updateClientService } from '../services/ClientService';
-  import { getProjects } from '../services/ProjectService';
-  import { useRoute, useRouter } from 'vue-router';
+  const client = clientResponse.data;
+  console.log('client', client);
   
-  const name = ref('');
-  const projects = ref([]);
-  const selectedProjects = ref([]);
-  const route = useRoute();
-  const router = useRouter();
+  // Certifique-se de que client.projects é um array antes de usar map
+  const projectClientIds = Array.isArray(client.projects) ? client.projects.map(project => project.id) : [];
   
-  onMounted(async () => {
-    const clientId = route.params.id;
-    const clientResponse = await getClient(clientId);
-    const projectResponse = await getProjects();
-    
-    const client = clientResponse.data;
-    name.value = client.name;
-    selectedProjects.value = client.projectIds;
-    projects.value = projectResponse.data;
+  name.value = client.name;
+  selectedProjects.value = projectClientIds;
+  projects.value = projectResponse.data;
+});
+
+const handleUpdateClient = async () => {
+  const clientId = route.params.id;
+  const updatedProjects = selectedProjects.value.map(projectId => {
+    const project = projects.value.find(proj => proj.id === projectId);
+    return { id: project.id, name: project.name };
   });
-  
-  const handleUpdateClient = async () => {
-    const clientId = route.params.id;
-    await updateClientService(clientId, { name: name.value, projectIds: selectedProjects.value });
-    router.push('/clients');
-  };
-  </script>
-  
+  await updateClientService(clientId, { name: name.value, projects: updatedProjects });
+  router.push('/clients');
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="container">
-    <h1>Project Management System</h1>
-    <router-link to="/clients" class="btn btn-primary">Manage Clients</router-link>
-    <router-link to="/projects" class="btn btn-primary">Manage Projects</router-link>
-    <router-link to="/activities" class="btn btn-primary">Manage Activities</router-link>
+    <div class="row flex-column">
+      <h1>Project Management System</h1>
+    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 mb-3">
+      <router-link to="/clients" class="btn btn-primary">Manage Clients</router-link>
+    </div>
+
+    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 mb-3">
+      <router-link to="/projects" class="btn btn-primary">Manage Projects</router-link>
+    </div>
+
+    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 mb-3">
+      <router-link to="/activities" class="btn btn-primary">Manage Activities</router-link>
+    </div>
+    </div>
   </div>
 </template>
 
